3901bb7b50153b2701c99fd3e98a0b5a5a4a6784
Diwas Thangarasu <diwasthangarasu@gmail.com>
2025-08-05 14:07:46 +0530

    Time: 0 ms (100%), Space: 41.5 MB (96.1%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.java
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -1,10 +1,12 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int count = 0;
-        for (int i = 0; i < stones.length(); i++) {
-            if (jewels.indexOf(stones.charAt(i)) != -1) {
-                count++;
-            }
+        int[] a = new int[58];
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            a[(int)stones.charAt(i)-65]++;
+        }
+        for(int i=0;i<jewels.length();i++){
+            count+=a[(int)jewels.charAt(i)-65];
         }
         return count;
     }
