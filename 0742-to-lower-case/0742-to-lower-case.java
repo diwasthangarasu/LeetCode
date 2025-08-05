841fdf078649ba49c4671f2e844f0d77a68fe229
Diwas Thangarasu <diwasthangarasu@gmail.com>
2025-08-05 13:30:29 +0530

    Time: 0 ms (100%), Space: 41.7 MB (61.22%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,12 +1,6 @@
 class Solution {
     public String toLowerCase(String s) {
-        String a="";
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)<91 && s.charAt(i)>64){
-                a+=(char)(s.charAt(i)+32);
-            }
-            else a+=s.charAt(i);
-        }
+        String a=s.toLowerCase();
         return a;
     }
 };
