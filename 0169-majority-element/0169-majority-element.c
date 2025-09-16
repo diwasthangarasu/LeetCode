7499885849b27963e383f0ad61130257ee25e602
Diwas Thangarasu <diwasthangarasu@gmail.com>
2025-09-16 09:21:15 +0530

    Time: 0 ms (100%), Space: 10.2 MB (39.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.c
+++ 0169-majority-element/0169-majority-element.c
@@ -1,6 +1,6 @@
 int majorityElement(int* nums, int numsSize) {
     int c=0;
-    int num=nums[0];
+    int num;
     for(int i=0;i<numsSize;i++){
         if(c==0) num=nums[i];
         if(nums[i]==num) c++;
