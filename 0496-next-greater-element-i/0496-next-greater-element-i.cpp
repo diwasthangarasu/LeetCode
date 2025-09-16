71cc1a03d3a51bd99a24675314b23e6fc80bc1c4
Diwas Thangarasu <diwasthangarasu@gmail.com>
2025-09-16 15:25:18 +0530

    Time: 1 ms (55.86%), Space: 12.8 MB (37.93%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -15,7 +15,7 @@ public:
             s.push(nums2[i]);
         }
         for(int i=0;i<n;i++){
-            if (mp.contains(nums1[i])) {
+            if (mp.find(nums1[i]) != mp.end()) {
                 res[i] = mp[nums1[i]];
             }
         }
