61a78d07649948afc1192efd81413bb60de3b157
Diwas Thangarasu <diwasthangarasu@gmail.com>
2025-09-17 14:14:27 +0530

    Time: 3 ms (18.46%), Space: 8.9 MB (80.4%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -2,20 +2,10 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         string res="";
-        int open=0;
+        int count=0;
         for(char c:s){
-            if(c=='('){
-                if(open>0){
-                    res.push_back(c);
-                }
-                open++;
-            }
-            else{
-                open--;
-                if(open>0) res.push_back(c);
-            }
-
-
+            if( c == '(' && count++) res+='(';
+            else if(c==')' && --count ) res+=')';
         }
         return res;
 
