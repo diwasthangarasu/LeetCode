80ea28152e09c164736cfdfd00d5f0d5776be3b3
Diwas Thangarasu <diwasthangarasu@gmail.com>
2025-09-17 15:58:32 +0530

    Time: 29 ms (22.86%), Space: 27.4 MB (23.69%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        map<string,vector<string>> mp;
+        for(string s:strs){
+            string ss=s;
+            sort(s.begin(),s.end());
+            if(mp.contains(s)){
+                mp[s].push_back(ss);
+            }
+            else{
+                vector<string> v;
+                v.push_back(ss);
+                mp[s]=v;
+            } 
+        }
+        for(pair<string,vector<string>> p:mp ){
+            res.push_back(p.second);
+        }
+        return res;
+    }
+};
