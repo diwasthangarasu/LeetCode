bae9f0e6d569f9029255bf4ffe3cc7020591736e
Diwas Thangarasu <diwasthangarasu@gmail.com>
2025-09-17 15:44:42 +0530

    Time: 7 ms (20.28%), Space: 9.9 MB (26.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return  s== t; 
+        map<char,int> mp;
+        for (char i:s){
+            mp[i]++;
+        }
+        for(char k: t){
+            mp[k]--;
+            if(mp[k]==0) mp.erase(k);
+        }
+        return mp.empty();
     }
 };
