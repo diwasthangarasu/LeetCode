cefef013d050c09fc6483186f49ee8b45ab7c05c
Diwas Thangarasu <diwasthangarasu@gmail.com>
2025-11-27 18:46:14 +0530

    Time: 0 ms (100%), Space: 46.6 MB (26.55%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null || head.next == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;         // move 1
+            fast = fast.next.next;    // move 2
+
+            if (slow == fast) {       // cycle detected
+                return true;
+            }
+        }
+
+        return false;  // fast hit null → no cycle
+    }
+}
