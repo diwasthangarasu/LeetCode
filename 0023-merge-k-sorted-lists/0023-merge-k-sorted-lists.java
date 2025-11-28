cc587a6e9a17e3dff3c57b1d4a6f8edc3c50fe8c
Diwas Thangarasu <diwasthangarasu@gmail.com>
2025-11-28 11:25:14 +0530

    Time: 82 ms (11.82%), Space: 46.9 MB (15.77%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0) return null;
+        ListNode head=lists[0];
+        for(int i=1;i<lists.length;i++){
+            head=sort(head,lists[i]);
+        }
+        return head;
+    }
+    public ListNode sort(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode(0);
+        ListNode temp=dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                temp.next=l1;
+                l1=l1.next;
+            }else{
+                temp.next=l2;
+                l2=l2.next;
+            }
+            temp=temp.next;
+        }
+        if(l1!=null) temp.next=l1;
+        if(l2!=null) temp.next=l2;
+        return dummy.next;
+    }
+}
