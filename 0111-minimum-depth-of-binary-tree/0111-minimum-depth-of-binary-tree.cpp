6a6d328137f7219e10b952d28f5ee5f44a466c7d
Diwas Thangarasu <diwasthangarasu@gmail.com>
2025-12-03 15:31:15 +0530

    Time: 6 ms (21.59%), Space: 147 MB (6.02%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -2,16 +2,10 @@ class Solution {
 public:
     int minDepth(TreeNode* root) {
         if (root == NULL) return 0;
-
-        // If left subtree is NULL, go only right
         if (root->left == NULL) 
             return minDepth(root->right) + 1;
-
-        // If right subtree is NULL, go only left
         if (root->right == NULL) 
             return minDepth(root->left) + 1;
-
-        // If both children exist
         return min(minDepth(root->left), minDepth(root->right)) + 1;
     }
 };
